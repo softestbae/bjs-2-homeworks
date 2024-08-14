0bef0dc81bd69931947c25795ba7d4c6d7c9ad59
Anna Mironova <mironova.aniuta@bk.ru>
2024-08-14 13:37:17 +0300

    Innitial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,24 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+  let countD = b**2-4*a*c
+
+  if (countD < 0) {
+    return arr;
+  }
+  else if (countD === 0) {
+    let onlyRoot = -b/(2*a);
+    arr.push(onlyRoot);
+
+    return arr;
+  }
+  else {
+    let fstRoot = (-b + Math.sqrt(d) )/(2*a);
+    let sndRoot = (-b - Math.sqrt(d) )/(2*a);
+
+    arr.push(fstRoot, sndRoot);
+    return arr;
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
