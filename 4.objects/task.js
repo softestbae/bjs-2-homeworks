5474bd92a4ddcdb448043e20cf5c24c19fff1ae8
Anna Mironova <mironova.aniuta@bk.ru>
2024-10-02 12:06:54 +0300

    task 4 done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,43 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
+let studentAndrew = new Student('Andrew', 'male', 27);
+let studentRose = new Student('Rose', 'female', 21);
+let studentBob = new Student('Bob', 'male', 19);
+let studentEmily = new Student('Emily', 'female', 31);
+
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if(this.marks) {
+        this.marks.push(...marks);
+      }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if(this.marks.length === 0) {
+    return 0;
+  }
+  else {
+    let sum = 0;
+    let count = 0;
+    for(let i = 0; i < this.marks.length; i++) {
+        sum += this.marks[i];
+        count += 1;
+    }
+    return sum / count;
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+
+  this.excluded = reason;
 }
