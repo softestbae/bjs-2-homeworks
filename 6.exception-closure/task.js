f28595066001e2bc4721673f52f158ae62c0718b
Anna Mironova <mironova.aniuta@bk.ru>
2024-10-05 14:57:54 +0300

    task 6

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -19,19 +19,29 @@ class Triangle {
         this.side1 = side1;
         this.side2 = side2;
         this.side3 = side3;
-    }
+        this.perimeter = null;
+        this.area = null;
 
-    checkTriangle() {
         if(this.side1 + this.side2 <= this.side3 || this.side1 + this.side3 <= this.side2 || this.side2 + this.side3 <= this.side1) {
-            throw new Error ('Треугольник с такими сторонами не существует';)
+            throw new Error ('Треугольник с такими сторонами не существует');
         }
     }
+    
+    perimeterAndArea() {
+      get perimeter() {
+        return this.side1 + this.side2 + this.side3;
+      }
 
-    set perimeter(side1, side2, side3) {
-       let _perimeter = this.side1 + this.side2 + this.side3;
+      get area() {
+        return (Math.sqrt(perimeter / 2 * (perimeter / 2 - side1)(perimeter / 2 - side2)(perimeter / 2 - side3))).toFixed(3);
+      }
     }
+}
 
-    get perimeter() {
-        return _perimeter;
+function getTriangle(side1, side2, side3) {
+    try { return Triangle 
+    } catch(error) { 
+        return perimeterAndArea;
+        console.log('Ошибка! Треугольник не существует');
     }
 }
